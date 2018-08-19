e36bb8b9cb1c4727584fa26baa4dd61f627ba562
Diego Fortunato <diego.fcandido1996@gmail.com>
2018-08-19 08:19:41 -0300

    Ajuste

--- src/main/java/com/marketMakers/model/UserPromotion.java
+++ src/main/java/com/marketMakers/model/UserPromotion.java
@@ -1,12 +1,18 @@
 package com.marketMakers.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "userPromotion")
 public class UserPromotion {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
     private String userId;
     private Long promotionId;
 	
