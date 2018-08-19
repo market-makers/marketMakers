96b4c6eda31f2a21f0ca95c0a52f7e2fe4bd4a65
Diego Fortunato <diego.fcandido1996@gmail.com>
2018-08-18 22:09:57 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/com/marketMakers/repository/UserRepository.java
+++ src/main/java/com/marketMakers/repository/UserRepository.java
@@ -3,5 +3,7 @@ package com.marketMakers.repository;
 import com.marketMakers.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import com.marketMakers.model.User;
+
 public interface UserRepository extends CrudRepository<User, String> {
 }
